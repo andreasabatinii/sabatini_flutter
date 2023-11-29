384b962f2bbab984f033e8417db82e0bbc121d67
andreasabatinii <a.sabatini@ied.edu>
2023-11-29 18:41:12 +0100

    feat (sidebar menu)

--- andrea_sabatini_flutter/lib/src/presentation/home/widgets/sidebar.dart
+++ andrea_sabatini_flutter/lib/src/presentation/home/widgets/sidebar.dart
@@ -1,38 +1,25 @@
+import 'package:andrea_sabatini_flutter/src/presentation/home/widgets/sidebar/upper_menu.dart';
+import 'package:andrea_sabatini_flutter/src/presentation/home/widgets/sidebar/user.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Sidebar extends StatelessWidget {
   const Sidebar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
+        color: Color(0xFF101010),
         width: 300,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(),
-                const Text('ChatGPT'),
-                const Spacer(),
-                const Icon(Icons.add),
-              ],
-            ),
-            Expanded(child: ListView()),
-            Row(
-              children: [
-                Container(
-                    margin: const EdgeInsets.only(left: 8),
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                          'url',
-                        ),
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                    ))
-              ],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(18),
+          child: Column(
+            children: [
+              UpperMenu(),
+              Expanded(child: ListView()),
+              User()
+            ],
+          ),
         ));
   }
 }
