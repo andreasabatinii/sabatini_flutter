266998be3cb0c4ff6de0225952c085acd1f4d71b
andreasabatinii <a.sabatini@ied.edu>
2024-01-13 15:20:59 +0100

    feat (layout creation)

--- andrea_sabatini_flutter/lib/src/presentation/main/main_page.dart
+++ andrea_sabatini_flutter/lib/src/presentation/main/main_page.dart
@@ -5,6 +5,8 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
